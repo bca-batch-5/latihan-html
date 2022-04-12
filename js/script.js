fa8ee2bc8a9e2c11fadb687348ac07909fd77988
femiliapm <femilia.putri@ui.ac.id>
2022-04-12 14:26:49 +0700

    js task solution

--- js/script.js
+++ js/script.js
@@ -1,70 +1,132 @@
-// console.log("test dari external file");
+// soal 1
+const name = "Femilia";
+const age = 25;
+const study = "Sarjana";
+const langProg = "HTML, CSS, JS, Java";
 
-var globalVariable = "variabel global yang bisa diubah nilainya";
-// console.log("teks");
-console.log("globalVariable", globalVariable);
-
-globalVariable = "teksnya diganti untuk global variable";
-console.log("globalVariable", globalVariable);
-console.log(typeof globalVariable);
-
-let email = "femiliapm@gmail.com";
-console.log("email", email);
-
-if (email === "femiliapm1@gmail.com") {
-  email = "test@gmail.com";
-  console.log("newEmail", email);
-} else {
-  console.log("error, email tidak cocok");
+function biodata(nama, umur, pend, progr) {
+  console.log("Nama: " + nama);
+  console.log(`Umur: ${umur}`);
+  console.log(`Pendidikan Terakhir: ${pend}`);
+  console.log(`Pemrograman yang pernah dipelajari: ${progr}`);
 }
 
-const birthDate = "20/02/2015";
-console.log("birthDate", birthDate);
-console.log("typeBirthDate", typeof birthDate);
+// biodata(name, age, study, langProg);
 
-// birthDate = '12/02/2000'
-// console.log("newBirthDate", birthDate); // akan menimbulkan error, karena type variable const
+// soal 2
 
-function penjumlahanDuaAngka(angka1, angka2) {
-  const bil1 = angka1;
-  const bil2 = angka2;
-  const res = bil1 + bil2;
-  console.log("hasil penjumlahan");
-  console.log(res);
-  console.log(typeof res);
+// soal 3
+function evenOdd(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} adalah bilangan genap`);
+  } else {
+    console.log(`${num} adalah bilangan ganjil`);
+  }
 }
 
-penjumlahanDuaAngka("10", 9);
+// evenOdd(29);
 
-console.log("Contoh penerapan for-loop");
-let nilaiAwal = 5;
-for (nilaiAwal; nilaiAwal < 10; nilaiAwal++) {
-  console.log(nilaiAwal);
-}
-console.log(nilaiAwal);
-
-one();
-
-function one() {
-  console.log("first");
-
-  setTimeout(() => {
-    two();
-  }, 0);
-
-  three();
+// soal 4
+function cekHarga(harga) {
+  if (harga >= 25000) {
+    console.log("Mahal");
+  } else if (harga > 10000) {
+    console.log("Sedang");
+  } else if (harga > 0 && harga <= 10000) {
+    console.log("Murah");
+  } else {
+    console.log("Input salah");
+  }
 }
 
-function two() {
-  console.log("second");
+// cekHarga(-210000);
+
+// soal 5 : mencetak angka 10-15
+// for (let i = 10; i <= 15; i++) {
+//   console.log(i);
+// }
+
+let i = 10;
+// while (i <= 15) {
+//   console.log(i);
+//   i++;
+// }
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 15);
+
+// soal 6
+function cekOngkir(jarak) {
+  if (jarak > 0 && jarak <= 2) {
+    let ongkosKirim = 5000;
+    return ongkosKirim;
+  } else if (jarak > 2) {
+    let selisihJarak = jarak - 2;
+    ongkosKirim = 3000 * selisihJarak;
+    let totalOngkir = 5000 + ongkosKirim;
+    return totalOngkir;
+  } else {
+    return "jarak tidak valid";
+  }
 }
 
-function three() {
-  console.log("third");
+// console.log(cekOngkir(-5));
+// 5000 + 9000 = 14000
+
+// soal 7
+// ketika bilangannya kelipatan 10, yg tampil nama saya
+function fazztrack(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("fazztrack");
+    } else if (i % 10 == 0) {
+      console.log("femil");
+    } else if (i % 3 == 0) {
+      console.log("fazz");
+    } else if (i % 5 == 0) {
+      console.log("track");
+    } else {
+      console.log(i);
+    }
+  }
 }
 
-// tambahan materi
-// promise
-// callback
-// async-await
-// sync
+// fazztrack(30);
+
+// soal 8
+function printSegitiga(num) {
+  let hasil = "";
+  // for (let i = num; i > 0; i--) {
+  for (let i = 0; i < num; i++) {
+    // for (let j = i; j > 0; j--) {
+    for (let j = 0; j <= i; j++) {
+      hasil += "* ";
+    }
+    hasil += "\n";
+  }
+
+  console.log(hasil);
+}
+
+// printSegitiga(5);
+
+// soal 9
+function printSquare(num) {
+  let hasil = "";
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      if (i == 0 || i == num - 1 || j == 0 || j == num - 1) {
+        hasil += "* ";
+      } else {
+        hasil += "  ";
+      }
+    }
+    hasil += "\n";
+  }
+
+  console.log(hasil);
+}
+
+printSquare(5);
